e7440caf32030fc2db0f03f30c47c5ff38623456
Aviral Goel <goel.aviral@gmail.com>
2019-07-17 17:37:00 -0400

    Fix entrypoint by backgrounding processes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup sudo service nginx start > /tmp/nginx.log
-nohup sudo Xvfb :4 -ac -screen 0 1960x2000x24 > /tmp/X.log
-export DISPLAY=:4
-xvfb-run bash
+nohup sudo service nginx start > /tmp/nginx.log &
+nohup sudo Xvfb :0 -ac -screen 0 1960x2000x24 > /tmp/X.log &
+export DISPLAY=:0
+bash
